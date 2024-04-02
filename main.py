d52e99713df18a3b9e0e7e0117b0b122a8625787
MihaiPopescu31 <mihai.gabrielpopescu1@gmail.com>
2024-04-02 15:26:29 +0300

    Adding the new functionality,to save the data in a file

--- main.py
+++ main.py
@@ -3,36 +3,45 @@ from tkinter import messagebox
 from tkinter import ttk
 
 def add_task():
-    # Add a task to the list
+    # Function to add a task to the list and save it to the file
     task = task_entry.get()
     if task:
         task_listbox.insert(tk.END, task)
         task_entry.delete(0, tk.END)
+        save_tasks_to_file()
     else:
         messagebox.showwarning("Warning", "Please enter a task!")
 
 def delete_task():
-    # Delete the selected task from the list
+    # Function to delete a task from the list and update the file
     try:
         selected_task_index = task_listbox.curselection()[0]
         task_listbox.delete(selected_task_index)
+        save_tasks_to_file()
     except IndexError:
         messagebox.showwarning("Warning", "Please select a task to delete!")
 
 def complete_task():
-    # Mark the selected task as completed
+    # Function to mark the selected task as completed
     try:
         selected_task_index = task_listbox.curselection()[0]
         task_listbox.itemconfig(selected_task_index, bg="light green")
     except IndexError:
         messagebox.showwarning("Warning", "Please select a task to complete!")
 
+def save_tasks_to_file():
+    # Function to save tasks to the tasks.txt file
+    tasks = task_listbox.get(0, tk.END)
+    with open("tasks.txt", "w") as file:
+        for task in tasks:
+            file.write(task + "\n")
+
 # Creating the main window
 root = tk.Tk()
 root.title("Todo List")
 root.geometry("400x300")
 
-# New style for buttons
+# Style for buttons
 new_style = ttk.Style()
 new_style.configure("New.TButton", foreground="white", background="#FF5733", font=('Helvetica', 12))
 
